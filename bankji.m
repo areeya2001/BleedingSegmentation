532326ea243b1156d8cdc0c4bba6e550fc1e178d
areeya2001 <areeya.seebutda@g.swu.ac.th>
2022-11-06 13:15:00 +0700

    Gray sale

--- bankji.m
+++ bankji.m
@@ -31,7 +31,7 @@ if createFolder == "Y"
                 %figure,imshow(bleeding)
                 clusterName = "cluster"+kmeanIndex;
                 clusterNameStr = sprintf('%s',clusterName);
-                imwrite(cluster,[clusterNameStr '.jpg']);
+                imwrite(cluster,[clusterNameStr '.png']);
             end
       end
 end
@@ -41,31 +41,56 @@ if isempty(createFolder)
     clusterFolder = dir('.\*');
     countFolderCluster = length(clusterFolder); 
     %  เปลี่ยนบรรทัดที่ 44 เป็น countFolderCluster หากต้องการที่จะเอาทุกโฟล์เดอร์
-    for folderClusterIndex = 3:countFolderCluster
+    for folderClusterIndex = 5:5
         clusterFolderPath = clusterFolder(folderClusterIndex).name;
         intoFolderEachCluster = rootPath+"\"+clusterFolderPath;
         cd(intoFolderEachCluster)
 
-        clusterInFolder = imageDatastore("./*.jpg");
+        clusterInFolder = imageDatastore("./*.png");
         imageInClusterFolder = readall(clusterInFolder);
         fprintf('Folder %s',intoFolderEachCluster);
         fprintf('\n')
         for redPixelCluster = 1:4
             imgInFolder = readimage(clusterInFolder,redPixelCluster);
-            
+            % find Red pixel each cluster
             redPoints = imgInFolder(:,:,1)>=130 & imgInFolder(:,:,2)<=60 & imgInFolder(:,:,3)<=100;
             percentRed = 100*(sum(sum(redPoints))/(size(imgInFolder,1)*size(imgInFolder,2)));
 
             if(percentRed >= 0.50)
-                mkdir assumeBleeding
-                copyfile(sprintf('cluster%d.jpg',redPixelCluster),'./assumeBleeding/')
+                  if ~exist('./assumeBleeding', 'dir')
+                       mkdir assumeBleeding
+                       copyfile(sprintf('cluster%d.png',redPixelCluster),'./assumeBleeding/')
+                  end
             end
             fprintf('Image has %d red pixels\n',sum(sum(redPoints)))
             fprintf('Image is %.2f percent red\n',percentRed)
             rgbRed1 = uint8(cat(3,redPoints,redPoints,redPoints)).*imgInFolder;
         end
+            cd('./assumeBleeding')
+            assumeBleedingFolder = imageDatastore("./*.png");
+            countAssumeBleedingFolder = length(assumeBleedingFolder);
+            imageInAssumeBleedingFolder = readall(assumeBleedingFolder);
 
-        cd("../")
+            for imageBleeding = 1:countAssumeBleedingFolder
+
+                imageBleedingInFolder = readimage(assumeBleedingFolder,2);
+                
+                imageBleeding2gray = rgb2gray(imageBleedingInFolder);
+
+                level = graythresh(imageBleeding2gray);
+                disp(level)
+                figure,imshow(imageBleeding2gray)
+
+                
+
+
+            end
+          
+           
+%          จบ loop 4 ครั้งอ่าน cluster 4 cluster
+
+%            อย่าลืมเปิดหากต้องการทำทุกโฟล์เดอร์
+%         cd("../")
     end
 
 end
